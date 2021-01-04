7e3a36bf07ccfd0f422a60150d2915c73163e524
Rohan Talip <RohanTalip@users.noreply.github.com>
2021-01-04 04:08:40 -0800

    test: add describe to a couple of tests and refactor them (#7233)
    Co-authored-by: Rohan Talip <rohantalip@blend.com>

--- test/github-issues/3158/issue-3158.ts
+++ test/github-issues/3158/issue-3158.ts
@@ -2,21 +2,23 @@ import "reflect-metadata";
 import { createTestingConnections, closeTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
 import { Connection } from "../../../src/connection/Connection";
 import { expect } from "chai";
-it("github issues > #3158 Cannot run sync a second time", async () => {
+
+describe("github issues > #3158 Cannot run sync a second time", async () => {
     let connections: Connection[];
-        connections = await createTestingConnections({
-            entities: [__dirname + "/entity/*{.js,.ts}"],
-            schemaCreate: true,
-            dropSchema: true,
-            enabledDrivers: ["mysql", "mariadb", "oracle", "mssql", "sqljs", "sqlite", "better-sqlite3"],
-            // todo(AlexMesser): check why tests are failing under postgres driver
-        });
-        await reloadTestingDatabases(connections);
-        await Promise.all(connections.map(async connection => {
-            const schemaBuilder = connection.driver.createSchemaBuilder();
-            const syncQueries = await schemaBuilder.log();
-            expect(syncQueries.downQueries).to.be.eql([]);
-            expect(syncQueries.upQueries).to.be.eql([]);
-        }));
-        await closeTestingConnections(connections);
+    before(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: true,
+        dropSchema: true,
+        enabledDrivers: ["mysql", "mariadb", "oracle", "mssql", "sqljs", "sqlite", "better-sqlite3"],
+        // todo(AlexMesser): check why tests are failing under postgres driver
+    }));
+    beforeEach(async () => await reloadTestingDatabases(connections));
+    after(async () => await closeTestingConnections(connections));
+
+    it("can recognize model changes", () => Promise.all(connections.map(async connection => {
+        const schemaBuilder = connection.driver.createSchemaBuilder();
+        const syncQueries = await schemaBuilder.log();
+        expect(syncQueries.downQueries).to.be.eql([]);
+        expect(syncQueries.upQueries).to.be.eql([]);
+    })));
 });
--- test/github-issues/3588/issue-3588.ts
+++ test/github-issues/3588/issue-3588.ts
@@ -2,20 +2,22 @@ import "reflect-metadata";
 import { createTestingConnections, closeTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
 import { Connection } from "../../../src/connection/Connection";
 import { expect } from "chai";
-it("github issues > #3588 Migration:generate issue with onUpdate using mysql 8.0", async () => {
+
+describe("github issues > #3588 Migration:generate issue with onUpdate using mysql 8.0", async () => {
     let connections: Connection[];
-        connections = await createTestingConnections({
-            entities: [__dirname + "/entity/*{.js,.ts}"],
-            schemaCreate: true,
-            dropSchema: true,
-            enabledDrivers: ["mysql"],
-        });
-        await reloadTestingDatabases(connections);
-        await Promise.all(connections.map(async connection => {
-            const schemaBuilder = connection.driver.createSchemaBuilder();
-            const syncQueries = await schemaBuilder.log();
-            expect(syncQueries.downQueries).to.be.eql([]);
-            expect(syncQueries.upQueries).to.be.eql([]);
-        }));
-        await closeTestingConnections(connections);
+    before(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: true,
+        dropSchema: true,
+        enabledDrivers: ["mysql"],
+    }));
+    beforeEach(async () => await reloadTestingDatabases(connections));
+    after(async () => await closeTestingConnections(connections));
+
+    it("can recognize model changes", () => Promise.all(connections.map(async connection => {
+        const schemaBuilder = connection.driver.createSchemaBuilder();
+        const syncQueries = await schemaBuilder.log();
+        expect(syncQueries.downQueries).to.be.eql([]);
+        expect(syncQueries.upQueries).to.be.eql([]);
+    })));
 });
