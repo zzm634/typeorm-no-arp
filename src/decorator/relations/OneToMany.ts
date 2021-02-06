b466edd756f2c38d4fc629a0bb02ebd1290fdd61
Rohan Talip <RohanTalip@users.noreply.github.com>
2021-02-06 02:17:33 -0800

    refactor: Updated comments for grammar and punctuation in OneToMany.ts (#7129)
    Co-authored-by: Rohan Talip <rohantalip@blend.com>
    Co-authored-by: AlexMesser <dmzt08@gmail.com>

--- src/decorator/relations/OneToMany.ts
+++ src/decorator/relations/OneToMany.ts
@@ -2,14 +2,15 @@ import {getMetadataArgsStorage, ObjectType, RelationOptions} from "../../";
 import {RelationMetadataArgs} from "../../metadata-args/RelationMetadataArgs";
 
 /**
- * One-to-many relation allows us to create a type of relation where Entity1 can have multiple instances of Entity2.
- * Entity2 has only one Entity1. Entity2 is the owner of the relationship and stores Entity1's id on its own side.
+ * A one-to-many relation allows creating the type of relation where Entity1 can have multiple instances of Entity2,
+ * but Entity2 has only one Entity1. Entity2 is the owner of the relationship, and stores the id of Entity1 on its
+ * side of the relation.
  */
 export function OneToMany<T>(typeFunctionOrTarget: string|((type?: any) => ObjectType<T>), inverseSide: string|((object: T) => any), options?: RelationOptions): PropertyDecorator {
     return function (object: Object, propertyName: string) {
         if (!options) options = {} as RelationOptions;
 
-        // now try to determine it its lazy relation
+        // Now try to determine if it is a lazy relation.
         let isLazy = options && options.lazy === true ? true : false;
         if (!isLazy && Reflect && (Reflect as any).getMetadata) { // automatic determination
             const reflectedType = (Reflect as any).getMetadata("design:type", object, propertyName);
