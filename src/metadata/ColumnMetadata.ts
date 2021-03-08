7fe723b23b74a4c81608a856a82b8aa85fe1b385
Json Choi <1890mah@gmail.com>
2021-03-08 22:10:11 +0900

    fix: correctly get referenceColumn value in `getEntityValueMap` (#7005)
    * test: add test case (#7002)
    
    * fix: correctly get referenceColumn value in `getEntityValueMap`

--- src/metadata/ColumnMetadata.ts
+++ src/metadata/ColumnMetadata.ts
@@ -544,9 +544,9 @@ export class ColumnMetadata {
             return Object.keys(map).length > 0 ? map : undefined;
 
         } else { // no embeds - no problems. Simply return column property name and its value of the entity
-            if (this.relationMetadata && entity[this.propertyName] && entity[this.propertyName] instanceof Object) {
+            if (this.relationMetadata && entity[this.relationMetadata.propertyName] && entity[this.relationMetadata.propertyName] instanceof Object) {
                 const map = this.relationMetadata.joinColumns.reduce((map, joinColumn) => {
-                    const value = joinColumn.referencedColumn!.getEntityValueMap(entity[this.propertyName]);
+                    const value = joinColumn.referencedColumn!.getEntityValueMap(entity[this.relationMetadata!.propertyName]);
                     if (value === undefined) return map;
                     return OrmUtils.mergeDeep(map, value);
                 }, {});
