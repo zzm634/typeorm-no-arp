50306cfb90883452c494fa14a226d796fd9c2fb2
James Ward <james@notjam.es>
2021-07-07 14:48:58 -0400

    refactor: use a `Map` for ConnectionManager to improve lookups (#7839)

--- src/connection/ConnectionManager.ts
+++ src/connection/ConnectionManager.ts
@@ -8,15 +8,19 @@ import {AlreadyHasActiveConnectionError} from "../error/AlreadyHasActiveConnecti
  * It also provides useful factory methods to simplify connection creation.
  */
 export class ConnectionManager {
-
-    // -------------------------------------------------------------------------
-    // Protected Properties
-    // -------------------------------------------------------------------------
-
     /**
      * List of connections registered in this connection manager.
+     *
+     * @deprecated
      */
-    public readonly connections: Connection[] = [];
+    get connections(): Connection[] {
+        return Array.from(Object.values(this.connectionMap));
+    }
+
+    /**
+     * Internal lookup to quickly get from a connection name to the Connection object.
+     */
+    private readonly connectionMap: Map<string, Connection> = new Map();
 
     // -------------------------------------------------------------------------
     // Public Methods
@@ -26,7 +30,7 @@ export class ConnectionManager {
      * Checks if connection with the given name exist in the manager.
      */
     has(name: string): boolean {
-        return !!this.connections.find(connection => connection.name === name);
+        return this.connectionMap.has(name);
     }
 
     /**
@@ -35,7 +39,7 @@ export class ConnectionManager {
      * Throws error if connection with the given name was not found.
      */
     get(name: string = "default"): Connection {
-        const connection = this.connections.find(connection => connection.name === name);
+        const connection = this.connectionMap.get(name);
         if (!connection)
             throw new ConnectionNotFoundError(name);
 
@@ -47,22 +51,17 @@ export class ConnectionManager {
      * Connection won't be established, you'll need to manually call connect method to establish connection.
      */
     create(options: ConnectionOptions): Connection {
-
         // check if such connection is already registered
-        const existConnection = this.connections.find(connection => connection.name === (options.name || "default"));
+        const existConnection = this.connectionMap.get(options.name || "default");
         if (existConnection) {
-
             // if connection is registered and its not closed then throw an error
             if (existConnection.isConnected)
                 throw new AlreadyHasActiveConnectionError(options.name || "default");
-
-            // if its registered but closed then simply remove it from the manager
-            this.connections.splice(this.connections.indexOf(existConnection), 1);
         }
 
         // create a new connection
         const connection = new Connection(options);
-        this.connections.push(connection);
+        this.connectionMap.set(connection.name, connection);
         return connection;
     }
 
