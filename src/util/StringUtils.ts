eb680f99b74c335556d23016264fcf1ea6ce1d6f
James Ward <james@notjam.es>
2021-07-10 17:59:01 -0400

    fix: handle snake case of ABcD which should become a_bc_d (#7883)
    this also simplifies the regular expressions

--- src/util/StringUtils.ts
+++ src/util/StringUtils.ts
@@ -18,8 +18,13 @@ export function camelCase(str: string, firstCapital: boolean = false): string {
  *
  * @see https://regex101.com/r/QeSm2I/1
  */
-export function snakeCase(str: string) {
-    return str.replace(/(?:([a-z])([A-Z]))|(?:((?!^)[A-Z])([a-z]))/g, "$1_$3$2$4").toLowerCase();
+export function snakeCase(str: string): string{
+    return str
+        // ABc -> ab_c
+        .replace(/([A-Z])([A-Z])([a-z])/g, "$1_$2$3")
+        // aC -> a_c
+        .replace(/([a-z])([A-Z])/g, "$1_$2")
+        .toLowerCase();
 }
 
 /**
