94f2616c5bb8248273548961f7332bc159bfd47f
Adrien Brunet <64930588+adrien-may@users.noreply.github.com>
2022-01-31 10:27:36 +0100

    docs: typo in Example comment sting --> string (#8550)

--- src/find-options/operator/Like.ts
+++ src/find-options/operator/Like.ts
@@ -2,7 +2,7 @@ import {FindOperator} from "../FindOperator";
 
 /**
  * Find Options Operator.
- * Example: { someField: Like("%some sting%") }
+ * Example: { someField: Like("%some string%") }
  */
 export function Like<T>(value: T|FindOperator<T>) {
     return new FindOperator("like", value);
