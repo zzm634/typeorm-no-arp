4590c9b996a1205f504ac453374ef4c2faab618b
Umed Khudoiberdiev <pleerock.me@gmail.com>
2022-02-17 18:49:14 +0500

    this test should not run if specific driver is disabled in ormconfig

--- test/github-issues/798/issue-798.ts
+++ test/github-issues/798/issue-798.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import * as assert from "assert";
 import {createConnection, getConnectionOptions} from "../../../src/index";
 import {Connection} from "../../../src/connection/Connection";
+import {getTypeOrmConfig} from "../../utils/test-utils";
 
 describe("github issues > #798 sqlite: 'database' path in ormconfig.json is not relative", () => {
     let connection: Connection;
@@ -22,6 +23,10 @@ describe("github issues > #798 sqlite: 'database' path in ormconfig.json is not 
     });
 
     it("should find the sqlite database if the cwd is changed", async function () {
+        // run test only if sqlite3 is enabled in ormconfig
+        const isEnabled = getTypeOrmConfig().some(conf => conf.type === "sqlite" && conf.skip === false)
+        if (!isEnabled) return
+
         const options = await getConnectionOptions("sqlite");
         connection = await createConnection(options);
 
@@ -29,6 +34,10 @@ describe("github issues > #798 sqlite: 'database' path in ormconfig.json is not 
     });
 
     it("should find the sqlite database if the cwd is changed for better-sqlite3", async function () {
+        // run test only if sqlite3 is enabled in ormconfig
+        const isEnabled = getTypeOrmConfig().some(conf => conf.type === "better-sqlite3" && conf.skip === false)
+        if (!isEnabled) return
+
         const options = await getConnectionOptions("better-sqlite3");
         connection = await createConnection(options);
 
