8ba742eb36586a21a918ed178208874a53ace3f9
Nelson Fleig <nfleig@hotmail.com>
2022-04-02 13:19:20 -0400

    fix: Update DeepPartial for usage of generics with Repository class (#8817)
    * test: test usage of generics with Repository class
    
    * test: cleanup
    
    * fix: update DeepPartial to work with generic Repository
    
    * fix: restore package-lock.json from typeorm/master

--- test/github-issues/8681/issue-8681.ts
+++ test/github-issues/8681/issue-8681.ts
@@ -1,13 +1,13 @@
+import { expect } from "chai"
+import { DataSource, DeepPartial, Repository } from "../../../src"
 import "../../utils/test-setup"
 import {
     closeTestingConnections,
     createTestingConnections,
     reloadTestingDatabases,
 } from "../../utils/test-utils"
-import { DataSource, DeepPartial } from "../../../src"
-import { expect } from "chai"
-import { Thing } from "./entity/thing.entity"
 import { Item } from "./entity/item.entity"
+import { Thing } from "./entity/thing.entity"
 
 describe("github issues > #8681 DeepPartial simplification breaks the .create() and .save() method in certain cases.", () => {
     let connections: DataSource[]
@@ -40,6 +40,35 @@ describe("github issues > #8681 DeepPartial simplification breaks the .create() 
                 expect(dbItems).to.have.length(2)
 
                 return { thing, items }
+            }),
+        ))
+    it("should .save() and .create() complex deep partial entities using a generic repository", () =>
+        Promise.all(
+            connections.map(async (connection) => {
+                class AbstractService<T> {
+                    private repository: Repository<T>
+                    constructor(target: any) {
+                        this.repository = new Repository(
+                            target,
+                            connection.manager,
+                        )
+                    }
+                    create(data: DeepPartial<T>): Promise<T> {
+                        const entity = this.repository.create(data)
+                        return this.repository.save(entity)
+                    }
+                }
+
+                const thingService = new AbstractService<Thing>(Thing)
+
+                const myThing: DeepPartial<Thing> = { id: 1 }
+                const thing = await thingService.create(myThing)
+
+                const thingRepository = connection.getRepository(Thing)
+                const dbItems = await thingRepository.find()
+                expect(dbItems).to.have.length(1)
+
+                return { thing }
             }),
         ))
 })
