07221a364682b567533c93130efb4f5189e009a9
ertl <klaus.ertl1@gmail.com>
2023-04-05 18:13:57 +0200

    fix: resolves issue with mssql column recreation (#9773)
    * fix: resolves issue with mssql column recreation when length max is in lower case
    
    Closes: #9399
    
    * removed redundant question mark
    
    ---------
    
    Co-authored-by: ke <ke@sbs.co.at>

--- src/driver/sqlserver/SqlServerDriver.ts
+++ src/driver/sqlserver/SqlServerDriver.ts
@@ -1042,7 +1042,10 @@ export class SqlServerDriver implements Driver {
         // of data type precedence to the expressions specified in the formula.
         if (columnMetadata.asExpression) return false
 
-        return tableColumn.length !== this.getColumnLength(columnMetadata)
+        return (
+            tableColumn.length.toUpperCase() !==
+            this.getColumnLength(columnMetadata).toUpperCase()
+        )
     }
 
     protected lowerDefaultValueIfNecessary(value: string | undefined) {
