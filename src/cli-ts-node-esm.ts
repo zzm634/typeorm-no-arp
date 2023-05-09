a188b1d9f4cc0bdc36a30be1380104e5f38ccb24
Sergey Pahuta <siarhei.pakhuta@mapbox.com>
2023-05-09 12:45:50 +0200

    fix: for running cli-ts-node-esm use exit code from child process (#10030)
    Use child process exit code in parent process to properly indicate that child process (migration running) fails
    
    Closes: #10029

--- src/cli-ts-node-esm.ts
+++ src/cli-ts-node-esm.ts
@@ -3,8 +3,8 @@ import { spawnSync } from "child_process"
 
 if ((process.env["NODE_OPTIONS"] || "").includes("--loader ts-node"))
     require("./cli")
-else
-    spawnSync(process.argv[0], process.argv.slice(1), {
+else {
+    const childProcess = spawnSync(process.argv[0], process.argv.slice(1), {
         stdio: "inherit",
         env: {
             ...process.env,
@@ -18,3 +18,6 @@ else
         },
         windowsHide: true,
     })
+
+    process.exit(childProcess.status || 0)
+}
